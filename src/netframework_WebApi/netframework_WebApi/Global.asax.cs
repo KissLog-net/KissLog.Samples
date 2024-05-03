414efeb17345b41b512de192703fb551506eb7cd
Catalin Gavan <catalingavan@gmail.com>
2024-05-04 00:07:51 +0200

    updated to KissLog 5.2.0

--- src/netframework_WebApi/netframework_WebApi/Global.asax.cs
+++ src/netframework_WebApi/netframework_WebApi/Global.asax.cs
@@ -39,9 +39,9 @@ namespace netframework_WebApi
         private void ConfigureKissLog()
         {
             KissLogConfiguration.Listeners
-                .Add(new RequestLogsApiListener(new Application(ConfigurationManager.AppSettings["KissLog.OrganizationId"], ConfigurationManager.AppSettings["KissLog.ApplicationId"]))
+                .Add(new RequestLogsApiListener(new Application(ConfigurationManager.AppSettings["LogBee.OrganizationId"], ConfigurationManager.AppSettings["LogBee.ApplicationId"]))
                 {
-                    ApiUrl = ConfigurationManager.AppSettings["KissLog.ApiUrl"]
+                    ApiUrl = ConfigurationManager.AppSettings["LogBee.ApiUrl"]
                 })
                 .Add(new LocalTextFileListener(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs")));
 
